61a3da67f89729e0d7321df4a54204d4be63b5e6
Ben Tucker <bwtucker4@gmail.com>
2022-04-13 15:37:39 -0700

    Add draw function that colors grid on mouseover. Add clear button that resets sketch grid.

--- main.js
+++ main.js
@@ -1,28 +1,34 @@
-// Set button clicked (8, 16, 32 or 64) to be the grid size
-// function setPixels() {
-// let btns = document.querySelectorAll("btn");
-//btns.forEach((btn) =>
-  //  btn.addEventListener("click", () => {
-    //  size = btn.id;
-    //})
-  //);
-//}
+function startSketch() {
+    populatePixels();
+    clearPixels();
+}
 
 // Create grid of divs
 function populatePixels () {
 let sketchContainer = document.querySelector(".sketch-container");
+let squares = sketchContainer.querySelectorAll("div");
+squares.forEach((div) => div.remove());
 sketchContainer.style.gridTemplateColumns = "repeat(16, 1fr)";
 sketchContainer.style.gridTemplateRows = "repeat(16, 1fr)";
 
-    for (let i = 0; i < 256; i++) {
-        let pixel = document.createElement("div");
-        pixel.style.backgroundColor = "lightgrey";
-        sketchContainer.insertAdjacentElement("beforeend", pixel);
+for (let i = 0; i < 256; i++) {
+    let pixel = document.createElement("div");
+    pixel.addEventListener("mouseover", () => {
+        pixel.style.backgroundColor = "black";
+    });
+    sketchContainer.insertAdjacentElement("beforeend", pixel);
     }
 }
 
-populatePixels();
+function clearPixels() {
+    let clearButton = document.querySelector(".clear-button");
+    clearButton.addEventListener("click", () => {
+        populatePixels();
+    });
+}
+
+startSketch();
 
 // Create a hover effect on pixels
 
-// When mouse over pixel, change div type to color
+// When mouse over pixel, change div type to selected color
