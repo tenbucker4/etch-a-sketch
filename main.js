05709bcfc1a5a4a7f0146872618a88f023f8eed9
Ben Tucker <bwtucker4@gmail.com>
2022-04-13 16:19:38 -0700

    Add input field to set grid size

--- main.js
+++ main.js
@@ -1,17 +1,25 @@
 function startSketch() {
-    populatePixels();
+    populatePixels(16);
     clearPixels();
 }
 
-// Create grid of divs
-function populatePixels () {
+function setGridSize(input) {
+    if (input >= 2 || input <= 100) {
+        populatePixels(input);
+    } else {
+        return;
+    }
+}
+
+function populatePixels (size) {
 let sketchContainer = document.querySelector(".sketch-container");
 let squares = sketchContainer.querySelectorAll("div");
 squares.forEach((div) => div.remove());
-sketchContainer.style.gridTemplateColumns = "repeat(16, 1fr)";
-sketchContainer.style.gridTemplateRows = "repeat(16, 1fr)";
+sketchContainer.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
+sketchContainer.style.gridTemplateRows = `repeat(${size}, 1fr)`;
 
-for (let i = 0; i < 256; i++) {
+let sizeSquared = size * size
+for (let i = 0; i < sizeSquared; i++) {
     let pixel = document.createElement("div");
     pixel.addEventListener("mouseover", () => {
         pixel.style.backgroundColor = "black";
@@ -28,7 +36,3 @@ function clearPixels() {
 }
 
 startSketch();
-
-// Create a hover effect on pixels
-
-// When mouse over pixel, change div type to selected color
