4b1e26cf8a3028d331445dfd4f3c175ee41db4df
Ben Tucker <bwtucker4@gmail.com>
2022-04-14 12:16:18 -0700

    Rework JS functions

--- main.js
+++ main.js
@@ -1,38 +1,62 @@
-function startSketch() {
-    populatePixels(16);
-    clearPixels();
+const DEFAULT_SIZE = 16;
+const DEFAULT_COLOR = "black";
+const DEFAULT_MODE = "colorMode"
+
+let currentSize = DEFAULT_SIZE;
+let currentColor = DEFAULT_COLOR;
+let currentMode = DEFAULT_MODE;
+
+const grid = document.getElementById("grid");
+const clearBtn = document.getElementById("clearBtn");
+const sizeSlider = document.getElementById("sizeSlider");
+const sizeValue = document.getElementById("sizeValue");
+const colorBtn = document.getElementById("colorBtn");
+const rainbowBtn = document.getElementById("rainbowBtn");
+const eraserBtn = document.getElementById("eraserBtn");
+
+clearBtn.onclick = () => resetGrid();
+sizeSlider.onmousemove = (e) => updateSizeValue(e.target.value)
+sizeSlider.onchange = (e) => changeSize(e.target.value)
+
+function changeSize(value) {
+    setSize(value);
+    updateSizeValue(value);
+    resetGrid();
 }
 
-function setGridSize(input) {
-    if (input >= 2 || input <= 100) {
-        populatePixels(input);
-    } else {
-        return;
+function updateSizeValue(value) {
+    sizeValue.textContent = `${value} x ${value}`;
+}
+
+function setSize(newSize) {
+    currentSize = newSize
+}
+
+function populateGrid(size) {
+    grid.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
+    grid.style.gridTemplateRows = `repeat(${size}, 1fr)`;
+
+    for (let i = 0; i < size * size; i++) {
+        const gridSquare = document.createElement("div");
+        gridSquare.classList.add("grid-element");
+        gridSquare.addEventListener("mouseover", draw)
+        grid.appendChild(gridSquare);
     }
 }
 
-function populatePixels (size) {
-let sketchContainer = document.querySelector(".sketch-container");
-let squares = sketchContainer.querySelectorAll("div");
-squares.forEach((div) => div.remove());
-sketchContainer.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
-sketchContainer.style.gridTemplateRows = `repeat(${size}, 1fr)`;
-
-let sizeSquared = size * size
-for (let i = 0; i < sizeSquared; i++) {
-    let pixel = document.createElement("div");
-    pixel.addEventListener("mouseover", () => {
-        pixel.style.backgroundColor = "black";
-    });
-    sketchContainer.insertAdjacentElement("beforeend", pixel);
-    }
+function draw(e) {
+    e.target.style.backgroundColor = "black";
 }
 
-function clearPixels() {
-    let clearButton = document.querySelector(".clear-button");
-    clearButton.addEventListener("click", () => {
-        populatePixels();
-    });
+function resetGrid() {
+    clearGrid();
+    populateGrid(currentSize);
 }
 
-startSketch();
+function clearGrid() {
+    grid.innerHTML = "";
+}
+
+window.onload = () => {
+    populateGrid(DEFAULT_SIZE);
+}
